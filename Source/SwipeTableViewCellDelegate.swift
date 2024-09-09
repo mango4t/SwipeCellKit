a4a3015ddfc5ddf278af0e293a0cc1d84b518436
Сергей Галездинов <segazero@gmail.com>
2024-09-09 12:42:32 +0300

    mark delegate as MainActor

--- Source/SwipeTableViewCellDelegate.swift
+++ Source/SwipeTableViewCellDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 /**
  The `SwipeTableViewCellDelegate` protocol is adopted by an object that manages the display of action buttons when the cell is swiped.
  */
+NS_SWIFT_UI_ACTOR
 public protocol SwipeTableViewCellDelegate: class {
     
     /**
